8b4ffbedb389253ffc9951d8a304cde402dd40d6
gabrielclv <gabrielcaualealvieira@gmail.com>
2025-08-14 16:37:33 -0300

    Resolve conflitos

---
+++ aula3/aula.js
@@ -0,0 +1,16 @@
+let n1 = 7 
+let n2 = 5 
+let n3 = 10
+let n4 = 10
+let media = (n1+n2*n3+n4)/4
+console.log(media)
+
+let n6 = 7 
+let n7 = 5 
+let n8 = 10
+let n9 = 10
+let numero = Math.sqrt(9)
+"Math.sqrt = raiz do seguinte numero"
+
+console.log(numero)
+
