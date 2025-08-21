d000ea9c9fd9877488bd1b9f2cf46051d1881414
Gabriel Cauã <gabrielcaualealvieira@gmail.com>
2025-08-21 08:50:12 -0300

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-"aprendemos sobre as variaveis string,float,char,int e bool "
-
-"let (variavel mutavel) e const (imutavel) "
-let n1 = 2 
-let n2 = 5
-
-"console.log = imprimir"
-console.log(n1+n2)
