6d6827454a7aed9d6aeab8e34fb560ff3828f7e6
Gabriel Cauã <gabrielcaualealvieira@gmail.com>
2025-09-05 16:05:50 -0300

    Correção e melhorias

--- aula5/atvd5.js
+++ aula5/atvd5.js
@@ -1,14 +1,16 @@
-let readline = require ('readline-sync');
+let readline = require('readline-sync');
 
 let nome = readline.question("Qual é o seu nome? ");
-let ano = readline.question("Em que ano você nasceu? ");
+let ano = Number(readline.question("Em que ano você nasceu? "));
 
+if (isNaN(ano)) {
+    console.log("Ano inválido. Digite um número.");
+} else {
+    let idade = 2025 - ano;
 
-idade = 2025 - ano
-
-
-if(idade>=18){
-    console.log("você pode ser preso")
-}else{
-    console.log("você é menor de idade")
+    if (idade >= 18) {
+        console.log("Você pode ser preso.");
+    } else {
+        console.log("Você é menor de idade.");
+    }
 }
